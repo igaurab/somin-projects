b833e426b9a0b5a1620d2fe2b00b9407c88679df
igaurab <dev@igaurab.com>
2021-08-01 16:41:21 +0545

    piano v1 completed

---
+++ js-piano/main.js
@@ -0,0 +1,39 @@
+const sa = document.querySelector('.sa')
+const re  = document.querySelector('.re')
+const ga = document.querySelector('.ga')
+const ma = document.querySelector('.ma')
+const pa = document.querySelector('.pa')
+const dha = document.querySelector('.dha')
+const ni = document.querySelector('.ni')
+
+const sa1 = document.querySelector('#sa')
+const re1 = document.querySelector('#re')
+const ga1 = document.querySelector('#ga')
+const ma1 = document.querySelector('#ma')
+const pa1 = document.querySelector('#pa')
+const dha1 = document.querySelector('#dha')
+const ni1 = document.querySelector('#ni')
+
+
+
+sa.addEventListener('click' , () =>{
+    sa1.play()
+})
+re.addEventListener('click' , () =>{
+    re1.play()
+})
+ga.addEventListener('click' , () =>{
+    ga1.play()
+})
+ma.addEventListener('click' , () =>{
+    ma1.play()
+})
+pa.addEventListener('click' , () =>{
+    pa1.play()
+})
+dha.addEventListener('click' , () =>{
+    dha1.play()
+})
+ni.addEventListener('click' , () =>{
+    ni1.play()
+})
