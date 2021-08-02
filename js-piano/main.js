b6b0e49a0bb6aafa852adde89c71b669aa54ce71
igaurab <dev@igaurab.com>
2021-08-02 21:42:22 +0545

    play sound on keypress

--- js-piano/main.js
+++ js-piano/main.js
@@ -1,3 +1,4 @@
+// Piano key selectors
 const sa = document.querySelector('.sa')
 const re  = document.querySelector('.re')
 const ga = document.querySelector('.ga')
@@ -6,6 +7,13 @@ const pa = document.querySelector('.pa')
 const dha = document.querySelector('.dha')
 const ni = document.querySelector('.ni')
 
+const sa2 = document.querySelector('.sa2')
+const re2 = document.querySelector('.re2')
+const ma2 = document.querySelector('.ma2')
+const pa2 = document.querySelector('.pa2')
+const dha2 = document.querySelector('.dha2')
+
+// Audio files id
 const sa1 = document.querySelector('#sa')
 const re1 = document.querySelector('#re')
 const ga1 = document.querySelector('#ga')
@@ -14,23 +22,38 @@ const pa1 = document.querySelector('#pa')
 const dha1 = document.querySelector('#dha')
 const ni1 = document.querySelector('#ni')
 
-
-const sa2 = document.querySelector('.sa2')
-const re2 = document.querySelector('.re2')
-const ma2 = document.querySelector('.ma2')
-const pa2 = document.querySelector('.pa2')
-const dha2 = document.querySelector('.dha2')
-
-
 const Db = document.querySelector('#Db')
 const Ab  = document.querySelector('#Ab') 
 const Eb  = document.querySelector('#Eb') 
 const Gb = document.querySelector('#Gb')
 const Bb = document.querySelector('#Bb')
 
+const audio_map = {
+    'a' : sa1,
+    's' : re1,
+    'd' : ga1,
+    'f' : ma1,
+    'g': pa1,
+    'h': dha1,
+    'j': ni1,
+    'z' : Db,
+    'x' : Ab,
+    'c' : Eb,
+    'v' : Gb,
+    'b': Bb
+}
 
 
-sa2.addEventListener('click' , () =>{
+const val = Object.getOwnPropertyNames(audio_map)
+document.addEventListener('keydown', (event)=>{
+    const key = event.key
+    if (audio_map.hasOwnProperty(key)) {
+        audio_map[key].play()
+    } 
+});
+
+
+sa2.addEventListener('keydown' , () =>{
     Db.play()
 })
 re2.addEventListener('click' , () =>{
